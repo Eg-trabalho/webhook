4a4b52f113b4f3020e248dea89d661e1bb6fbff4
admin <admin@admin.com>
2025-01-13 11:07:35 -0300

    Csvv

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import whatsapp_webhook, read_csv_and_send_messages, ver_numeros, del_numeros, ver_csv
+from .views import whatsapp_webhook, read_csv_and_send_messages, ver_numeros, del_numeros, ver_csv, ver_csv_inteiro
 
 urlpatterns = [
     path("v1/", whatsapp_webhook, name="whatsapp_webhook"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("numeros/", ver_numeros, name="ver_numeros"),
     path("del_numeros/", del_numeros, name="del_numeros"),
     path("csv/", ver_csv, name="ver_csv"),
+    path("csv_inteiro/", ver_csv_inteiro, name="ver_csv_inteiro"),
 ]
